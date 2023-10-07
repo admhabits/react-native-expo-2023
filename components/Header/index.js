5e4f556dffaea53352f1888004fd1be868565b5e
Alam Wibowo <87102127+admhabits@users.noreply.github.com>
2023-10-07 07:21:41 +0000

    Add Logo Dark Mode

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,16 +1,28 @@
-import { StyleSheet, Text } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import { logoHubla, iconSearch } from "../configs/Images";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
 
 export default function Header({ style }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <>
       <LinearGradient
         colors={["#28A297", "rgba(40, 162, 151, 0.5)"]}
-        style={{  ...styles.header, ...style }}
+        style={{ ...styles.header, ...style }}
       >
-        <Text style={styles.title}>Home Page</Text>
-        <Text style={styles.title}>Home Page</Text>
+        <View style={styles.brand}>
+          <Image source={logoHubla} style={styles.logo} />
+          <Text style={styles.title}>KEMENTRIAN PERHUBUNGAN LAUT</Text>
+        </View>
+        <TouchableOpacity onPress={() => setIsDarkMode(!isDarkMode)}>
+          <FontAwesome
+            name={isDarkMode ? "sun" : "moon"}
+            size={20}
+            color={isDarkMode ? "#fff" : "rgba(40, 162, 151, 1)"}
+          />
+        </TouchableOpacity>
       </LinearGradient>
     </>
   );
@@ -23,6 +35,18 @@ const styles = StyleSheet.create({
     gap: 8,
     justifyContent: "space-between",
     alignItems: "center",
+  },
+
+  brand: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+  },
+
+  logo: {
+    width: 30,
+    height: 31,
+    marginRight: 5,
   },
 
   title: {
