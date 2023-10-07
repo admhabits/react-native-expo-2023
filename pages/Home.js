1ed66fb269e9bd4f9ca779ca6129f63f0b159242
Alam Wibowo <87102127+admhabits@users.noreply.github.com>
2023-10-07 04:39:41 +0000

    First Style

--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>Home Page</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 export default Home
