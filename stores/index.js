1934d2c4158848c5413f949ea1663ce3f1432a3d
Alam Wibowo <alamhafidz61@gmail.com>
2023-10-10 23:45:07 +0700

    Create Auth Slice

--- stores/index.js
+++ stores/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./slices/CounterSlice";
+import AuthSlice from './slices/AuthSlice';
 
 export const store = configureStore({
   reducer: { counter: CounterReducer },
---
+++ stores/slices/AuthSlice.js
@@ -0,0 +1,32 @@
+// authSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+    rememberMe: false,
+  },
+  reducers: {
+    loginUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+      state.rememberMe = false;
+    },
+    toggleRememberMe: (state) => {
+      state.rememberMe = !state.rememberMe;
+    },
+  },
+});
+
+export const { loginUser, logoutUser, toggleRememberMe } = authSlice.actions;
+export const selectUser = (state) => state.auth.user;
+export const selectIsAuthenticated = (state) => state.auth.isAuthenticated;
+export const selectRememberMe = (state) => state.auth.rememberMe;
+
+export default authSlice.reducer;
