89d6e18cdba373007d1601e46c0022a71add8bc0
Alam Wibowo <alamhafidz61@gmail.com>
2023-10-10 23:57:07 +0700

    Add Config Absolute Import

--- routes/screen.js
+++ routes/screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "../pages/Home";
+import Home from "~/pages/Home";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
