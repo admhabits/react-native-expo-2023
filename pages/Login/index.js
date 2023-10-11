4301c2d9717bbefee042fcbf0950e18776299634
Alam Wibowo <alamhafidz61@gmail.com>
2023-10-12 00:10:19 +0700

    Add Login Screen

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,6 +1,6 @@
 //import liraries
 import { toscaColor } from "~/components/configs/Colors";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -8,12 +8,15 @@ import {
   BackHandler,
   Alert,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { loginUser } from "~/stores/slices/AuthSlice";
 import { toggleRememberMe } from "~/stores/slices/AuthSlice";
 import { saveState } from "~/stores/saved/AsyncStorage";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { PaddingLayout } from "~/components/configs/Layout";
 
 // create a component
 const Login = () => {
@@ -21,16 +24,17 @@ const Login = () => {
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
 
-  const LoginHandler = () => {
-    dispatch(
-      loginUser({
-        username: "@alamhafidz",
-      })
-    );
-    if (true) { //Ketika checked RememberMe
-      dispatch(toggleRememberMe());
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const LoginHandler = ({ username, password }) => {
+    if (username == "" || password == "") {
+      Alert.alert("Masukan Username dan Password yang valid!");
+    } else {
+      dispatch(loginUser({ username }));
+      if (true) dispatch(toggleRememberMe());
+      saveState(auth);
     }
-    saveState(auth);
   };
 
   useEffect(() => {
@@ -58,21 +62,37 @@ const Login = () => {
   }, [isAuthenticated]);
 
   return (
-    <View style={styles.container}>
-      <Text>Login Page</Text>
-      <TouchableOpacity onPress={() => LoginHandler()}>
-        <Text
-          style={{
-            color: "white",
-            padding: 10,
-            margin: 10,
-            backgroundColor: "orange",
-          }}
-        >
-          Login App
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TextInput
+          onChange={(e) => setUsername(e.value)}
+          placeholder="Username"
+          style={styles.input}
+        ></TextInput>
+        <TextInput
+          secureTextEntry={true}
+          onChange={(e) => setPassword(e.value)}
+          placeholder="Password"
+          style={styles.input}
+        ></TextInput>
+        <TouchableOpacity onPress={() => LoginHandler({ username, password })}>
+          <Text
+            style={{
+              color: "white",
+              padding: 15,
+              marginTop: 10,
+              fontFamily: "Montserrat",
+              backgroundColor: "orange",
+              fontSize: 18,
+              textAlign: "center",
+              borderRadius: 10,
+            }}
+          >
+            Masuk
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -81,8 +101,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor: toscaColor,
+    padding: PaddingLayout + 30,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    fontFamily: "Montserrat",
+    borderRadius: 8,
+    marginVertical: 10,
+    fontSize: 14,
   },
 });
 
