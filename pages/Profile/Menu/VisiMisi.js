d70d7d3e560e749e7308313e99520c633b7995a4
Alam Wibowo <alamhafidz61@gmail.com>
2023-10-30 22:38:35 +0700

    Slicing Layout Menu All Profile Detail

---
+++ pages/Profile/Menu/VisiMisi.js
@@ -0,0 +1,35 @@
+//import liraries
+import Layout from '~/components/Layout/General';
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+
+// create a component
+const VisiMisi = () => {
+    return (
+        <Layout
+        statusBar={toscaColor}
+        navigation={navigation}
+        titlePage={"Visi & Misi"}
+      >
+        <ScrollView
+          style={{ padding: 10, marginTop: 20 }}
+          showsVerticalScrollIndicator={false}
+        >
+          Visi dan Misi
+        </ScrollView>
+      </Layout>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
+
+//make this component available to the app
+export default VisiMisi;
